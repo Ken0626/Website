060c8a0fa4adac163827e41733cf8245ad56f098
TheMoodyOldman <anthonyfu2005@gmail.com>
2023-05-26 11:49:33 +0800

    model

--- dazhi_network_setting/main/models.py
+++ dazhi_network_setting/main/models.py
@@ -2,11 +2,14 @@ from django.db import models
 
 # Create your models here.
 
-class User(models.Model):
-    pass
-
-class Device(models.Model):
-    pass
-
 class Group(models.Model):
-    pass
+    g_name = models.CharField("班級/群組")
+
+class User(models.Model):
+    name = models.CharField("使用者姓名", max_length=10)
+    groups = models.ForeignKey(Group, models.CASCADE)
+    
+class Device(models.Model):
+    d_name = models.CharField("裝置型號", max_length=25)
+    owner = models.ForeignKey(User)
+
