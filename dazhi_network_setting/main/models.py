f820b759713d0f800f819ec6e70ecf4da6407e2f
TheMoodyOldman <anthonyfu2005@gmail.com>
2023-06-09 11:06:36 +0800

    things

--- dazhi_network_setting/main/models.py
+++ dazhi_network_setting/main/models.py
@@ -1,15 +1,16 @@
 from django.db import models
+from django.contrib.auth.models import User
 
 # Create your models here.
 
 class Group(models.Model):
-    g_name = models.CharField("班級/群組")
+    g_name = models.CharField("班級/群組", max_length=10)
 
-class User(models.Model):
-    name = models.CharField("使用者姓名", max_length=10)
-    groups = models.ForeignKey(Group, models.CASCADE)
+# class User(models.Model):
+#     name = models.CharField("使用者姓名", max_length=10)
+#     groups = models.ForeignKey(Group, models.CASCADE)
     
 class Device(models.Model):
     d_name = models.CharField("裝置型號", max_length=25)
-    owner = models.ForeignKey(User)
+    owner = models.ForeignKey(User, models.CASCADE)
 
