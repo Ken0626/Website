2079759563abf6d75946ba31c160babbb07f85a1
Ken0626 <kenyang0626@gmail.com>
2023-06-14 22:48:27 +0800

    將網站的連結對應的template大致完成

--- dazhi_network_setting/main/urls.py
+++ dazhi_network_setting/main/urls.py
@@ -3,4 +3,9 @@ from .views import *
 
 urlpatterns = [
     path('', Home.as_view()),
+    path('device_edit_t/',EditDevice_t.as_view()),
+    path('device_edit/',DeviceAdd.as_view()),
+    path('device_list/',DeviceList.as_view()),
+    path('user_edit_t/',EditUser_t.as_view()),
+    path('user_edit/',UserAdd.as_view()),
 ]
