2c620a938dcda0298c79fa97790ec9944665a48a
TheMoodyOldman <anthonyfu2005@gmail.com>
2023-06-16 10:19:59 +0800

    something

--- dazhi_network_setting/main/views.py
+++ dazhi_network_setting/main/views.py
@@ -7,6 +7,7 @@ from .models import *
 from django.contrib.auth.models import User
 from django.contrib.auth.mixins import LoginRequiredMixin
 
+
 from .forms import GetExcelForm
 
 # Create your views here.
@@ -26,6 +27,10 @@ class DeviceAdd(LoginRequiredMixin, CreateView):
     fields = ["d_name" , "MAC"]
 
     success_url = '../list/'
+
+    def form_valid(self, form):
+        form.instance.owner = self.request.user
+        return super().form_valid(form)
 
 class DeviceDetail(DetailView):
     model = Device
@@ -68,9 +73,10 @@ class UserAdd(LoginRequiredMixin, CreateView):
         return form
     
     def form_vaild(self, form):
-        number = form.files['username']
-        form.instence.password
-        return super.form_vaild(form)
+        user = form.save(commit=False)
+        user.set_password(form.instance.username)
+        return super().form_valid(form)
+
 
     
 class UserList(LoginRequiredMixin, ListView):
@@ -80,11 +86,39 @@ class UserList(LoginRequiredMixin, ListView):
 class Upload(LoginRequiredMixin, FormView):
     template_name = 'form.html'
     form_class = GetExcelForm
+    success_url = ''
 
-    def get_form(self):
-        form = super().get_form()
-        return form
+    # def get_form(self):
+    #     form = super().get_form()
+    #     return form
     
     def form_valid(self, form):
-        s = form.instance.file
+        print("!!!!")
+        s = form.files['file']
+        ext = s.name.split('.')[-1]
+        content = s.read()
+        sheets = px.get_records(file_type=ext, file_content=content)
+
+        for sheet in sheets:
+            exists_groups = list(Group.objects.all())
+            # group = Group(g_name=sheet['所屬班級/單位'])
+            match_group = list(filter(lambda g: g.g_name == str(sheet['所屬班級/單位']), exists_groups))
+            if match_group:
+                group = match_group[0]
+            else:
+                group = Group(g_name=sheet['所屬班級/單位'])
+                group.save()
+
+            try:
+                user = User(first_name=sheet['學生/教師姓名'], 
+                            username=sheet['學號/教師代碼'])
+                user.set_password(str(sheet['學號/教師代碼']))
+                user.save()
+                # user.groups.add(group)
+            except:
+                user = User.objects.get(username=sheet['學號/教師代碼'])
+                # user.groups.add(group)
+                user.save()
+        return super().form_valid(form)
+
 
